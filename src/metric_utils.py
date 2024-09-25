cae54887467202ca69cd032853b1dd72aece05af
Adeel Akram <adeel.chep@gmail.com>
2024-09-25 14:58:27 +0200

    ran Black formatter on src

--- src/metric_utils.py
+++ src/metric_utils.py
@@ -9,9 +9,23 @@ import matplotlib.pyplot as plt
 import sklearn.metrics
 
 # Define our Metrics class as a namedtuple
-Metrics = namedtuple('Metrics', ['accuracy', 'precision', 'recall', 'f1',
-                                 'prc_precision', 'prc_recall', 'prc_thresh', 'prc_auc',
-                                 'roc_fpr', 'roc_tpr', 'roc_thresh', 'roc_auc'])
+Metrics = namedtuple(
+    "Metrics",
+    [
+        "accuracy",
+        "precision",
+        "recall",
+        "f1",
+        "prc_precision",
+        "prc_recall",
+        "prc_thresh",
+        "prc_auc",
+        "roc_fpr",
+        "roc_tpr",
+        "roc_thresh",
+        "roc_auc",
+    ],
+)
 
 
 def compute_metrics(preds, targets, threshold=0.5):
@@ -22,10 +36,14 @@ def compute_metrics(preds, targets, threshold=0.5):
     accuracy = sklearn.metrics.accuracy_score(y_true, y_pred)
     # precision = sklearn.metrics.precision_score(y_true, y_pred)
     # recall = sklearn.metrics.recall_score(y_true, y_pred)
-    precision, recall, f1, support = sklearn.metrics.precision_recall_fscore_support(y_true, y_pred, average='binary')
+    precision, recall, f1, support = sklearn.metrics.precision_recall_fscore_support(
+        y_true, y_pred, average="binary"
+    )
 
     # Precision-Recall Curve
-    prc_precision, prc_recall, prc_thresh = sklearn.metrics.precision_recall_curve(y_true, preds)
+    prc_precision, prc_recall, prc_thresh = sklearn.metrics.precision_recall_curve(
+        y_true, preds
+    )
     prc_auc = sklearn.metrics.auc(prc_recall, prc_precision)
 
     # ROC Curve
@@ -33,13 +51,24 @@ def compute_metrics(preds, targets, threshold=0.5):
     roc_auc = sklearn.metrics.auc(roc_fpr, roc_tpr)
 
     # Organize metrics into a namedtuple
-    metrics = Metrics(accuracy=accuracy, precision=precision, recall=recall, f1=f1,
-                      prc_precision=prc_precision, prc_recall=prc_recall, prc_thresh=prc_thresh, prc_auc=prc_auc,
-                      roc_fpr=roc_fpr, roc_tpr=roc_tpr, roc_thresh=roc_thresh, roc_auc=roc_auc)
+    metrics = Metrics(
+        accuracy=accuracy,
+        precision=precision,
+        recall=recall,
+        f1=f1,
+        prc_precision=prc_precision,
+        prc_recall=prc_recall,
+        prc_thresh=prc_thresh,
+        prc_auc=prc_auc,
+        roc_fpr=roc_fpr,
+        roc_tpr=roc_tpr,
+        roc_thresh=roc_thresh,
+        roc_auc=roc_auc,
+    )
     return metrics
 
 
-def plot_metrics(preds, targets, metrics, name="gnn", scale='linear'):
+def plot_metrics(preds, targets, metrics, name="gnn", scale="linear"):
     # Prepare the values
     labels = targets > 0.5
 
@@ -47,25 +76,34 @@ def plot_metrics(preds, targets, metrics, name="gnn", scale='linear'):
     fig, (ax0, ax1, ax2) = plt.subplots(ncols=3, figsize=(16, 5))
 
     # Plot Model Outputs
-    binning = dict(bins=25, range=(0, 1), histtype='step', log=True)
-    ax0.hist(preds[labels == True], label='Real Edges', **binning)
-    ax0.hist(preds[labels == False], label='Fake Edges', **binning)
-    ax0.set_xlabel('Model Output', size=14)
-    ax0.legend(loc='best')
+    binning = dict(bins=25, range=(0, 1), histtype="step", log=True)
+    ax0.hist(preds[labels == True], label="Real Edges", **binning)
+    ax0.hist(preds[labels == False], label="Fake Edges", **binning)
+    ax0.set_xlabel("Model Output", size=14)
+    ax0.legend(loc="best")
 
     # Plot Precision & Recall
-    ax1.plot(metrics.prc_thresh, metrics.prc_precision[:-1], label='Edge Purity')  # Purity
-    ax1.plot(metrics.prc_thresh, metrics.prc_recall[:-1], label='Edge Efficiency')  # Efficiency
-    ax1.set_xlabel('Edge Score Cut', size=14)  # Model Threshold
-    ax1.legend(loc='best')
+    ax1.plot(
+        metrics.prc_thresh, metrics.prc_precision[:-1], label="Edge Purity"
+    )  # Purity
+    ax1.plot(
+        metrics.prc_thresh, metrics.prc_recall[:-1], label="Edge Efficiency"
+    )  # Efficiency
+    ax1.set_xlabel("Edge Score Cut", size=14)  # Model Threshold
+    ax1.legend(loc="best")
 
     # Plot the ROC curve
-    ax2.plot(metrics.roc_fpr, metrics.roc_tpr, color="darkorange", label="AUC = %.5f" % metrics.roc_auc)
+    ax2.plot(
+        metrics.roc_fpr,
+        metrics.roc_tpr,
+        color="darkorange",
+        label="AUC = %.5f" % metrics.roc_auc,
+    )
     ax2.plot([0, 1], [0, 1], color="navy", linestyle="--")
-    ax2.set_xlabel('False Positive Rate', size=14)
-    ax2.set_ylabel('True Positive Rate', size=14)
+    ax2.set_xlabel("False Positive Rate", size=14)
+    ax2.set_ylabel("True Positive Rate", size=14)
     ax2.set_xscale(scale)
-    ax2.legend(loc='best')
+    ax2.legend(loc="best")
 
     fig.tight_layout()
     fig.savefig(name + "_metrics.pdf")
@@ -77,16 +115,18 @@ def plot_roc(metrics, name="gnn"):
     fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
 
     # ROC Curve with AUC
-    axs.plot(metrics.roc_fpr, metrics.roc_tpr, color="darkorange", label="GNN Classifier")
+    axs.plot(
+        metrics.roc_fpr, metrics.roc_tpr, color="darkorange", label="GNN Classifier"
+    )
     axs.plot([0, 1], [0, 1], color="navy", linestyle="--", label="Naive Classifier")
 
     # Axes Params
     axs.set_title("ROC Curve, AUC = %.5f" % metrics.roc_auc, fontsize=15)
-    axs.set_xlabel('False Positive Rate', size=20)
-    axs.set_ylabel('True Positive Rate', size=20)
-    axs.tick_params(axis='both', which='major', labelsize=12)
-    axs.tick_params(axis='both', which='minor', labelsize=12)
-    axs.legend(fontsize=14, loc='lower right')
+    axs.set_xlabel("False Positive Rate", size=20)
+    axs.set_ylabel("True Positive Rate", size=20)
+    axs.tick_params(axis="both", which="major", labelsize=12)
+    axs.tick_params(axis="both", which="minor", labelsize=12)
+    axs.legend(fontsize=14, loc="lower right")
 
     # Fig Params
     fig.tight_layout()
@@ -100,16 +140,21 @@ def plot_prc(metrics, name="gnn"):
     fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
 
     # ROC Curve with AUC
-    axs.plot(metrics.prc_recall, metrics.prc_precision, color="darkorange", label="GNN Classifier")
+    axs.plot(
+        metrics.prc_recall,
+        metrics.prc_precision,
+        color="darkorange",
+        label="GNN Classifier",
+    )
     axs.plot([0, 1], [0, 0], color="navy", linestyle="--", label="Naive Classifier")
 
     # Axes Params
     axs.set_title("PRC Curve, AUC = %.5f" % metrics.prc_auc, fontsize=15)
-    axs.set_xlabel('Recall', size=20)
-    axs.set_ylabel('Precision', size=20)
-    axs.tick_params(axis='both', which='major', labelsize=12)
-    axs.tick_params(axis='both', which='minor', labelsize=12)
-    axs.legend(fontsize=14, loc='center right')
+    axs.set_xlabel("Recall", size=20)
+    axs.set_ylabel("Precision", size=20)
+    axs.tick_params(axis="both", which="major", labelsize=12)
+    axs.tick_params(axis="both", which="minor", labelsize=12)
+    axs.legend(fontsize=14, loc="center right")
 
     # Fig Params
     fig.tight_layout()
@@ -131,11 +176,11 @@ def plot_prc_thr(metrics, name="gnn"):
 
     # Axes Params
     axs.set_title("Edge Scores vs Efficiency and Purity", fontsize=15)
-    axs.set_xlabel('Edge Score Cut', size=20)
-    axs.tick_params(axis='both', which='major', labelsize=12)
-    axs.tick_params(axis='both', which='minor', labelsize=12)
+    axs.set_xlabel("Edge Score Cut", size=20)
+    axs.tick_params(axis="both", which="major", labelsize=12)
+    axs.tick_params(axis="both", which="minor", labelsize=12)
     # axs.set_ylim(0.5,1.02)
-    axs.legend(fontsize=14, loc='lower center')
+    axs.legend(fontsize=14, loc="lower center")
 
     # Fig Params
     fig.tight_layout()
@@ -160,9 +205,9 @@ def plot_epc(metrics, name="gnn"):
     # axs.set_title("EP Curve, AUC = %.5f" % epc_auc, fontsize=15)
     axs.set_xlabel("Edge Efficiency", fontsize=24)
     axs.set_ylabel("Edge Purity", fontsize=24)
-    axs.tick_params(axis='both', which='major', labelsize=14)
-    axs.tick_params(axis='both', which='minor', labelsize=14)
-    axs.legend(loc='lower left', fontsize=20)
+    axs.tick_params(axis="both", which="major", labelsize=14)
+    axs.tick_params(axis="both", which="minor", labelsize=14)
+    axs.legend(loc="lower left", fontsize=20)
 
     # Fig Params
     fig.tight_layout()
@@ -192,10 +237,10 @@ def plot_epc_cut(metrics, name="gnn"):
     # Axes Params
     # axs.set_title("Edge Scores vs Efficiency and Purity", fontsize=15)
     axs.set_xlabel("Edge Score Cut", fontsize=24)
-    axs.tick_params(axis='both', which='major', labelsize=14)
-    axs.tick_params(axis='both', which='minor', labelsize=14)
+    axs.tick_params(axis="both", which="major", labelsize=14)
+    axs.tick_params(axis="both", which="minor", labelsize=14)
     # axs.set_ylim(0.5,1.02)
-    axs.legend(loc='lower center', fontsize=20)
+    axs.legend(loc="lower center", fontsize=20)
 
     # Fig Params
     fig.tight_layout()
@@ -210,18 +255,18 @@ def plot_output(preds, targets, threshold=0.5, name="gnn"):
     fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
 
     # Ploting
-    binning = dict(bins=25, range=(0, 1), histtype='step', log=True)
-    axs.hist(preds[labels == True], label='True Edges', **binning)  # True Edges
-    axs.hist(preds[labels == False], label='False Edges', **binning)  # False Edges
+    binning = dict(bins=25, range=(0, 1), histtype="step", log=True)
+    axs.hist(preds[labels == True], label="True Edges", **binning)  # True Edges
+    axs.hist(preds[labels == False], label="False Edges", **binning)  # False Edges
 
     # Axes Params
     # axs.set_title("Classifier Output", fontsize=15)
-    axs.set_xlabel('Model Output', size=20)
-    axs.set_ylabel('Counts', size=20)
-    axs.tick_params(axis='both', which='major', labelsize=14)
-    axs.tick_params(axis='both', which='minor', labelsize=14)
+    axs.set_xlabel("Model Output", size=20)
+    axs.set_ylabel("Counts", size=20)
+    axs.tick_params(axis="both", which="major", labelsize=14)
+    axs.tick_params(axis="both", which="minor", labelsize=14)
     # axs.set_ylim(ymin=.005)
-    axs.legend(loc='upper center', fontsize=16)
+    axs.legend(loc="upper center", fontsize=16)
 
     # Fig Params
     fig.tight_layout()
@@ -230,37 +275,36 @@ def plot_output(preds, targets, threshold=0.5, name="gnn"):
 
 # Draw Prediction Sample
 def draw_sample_xy(hits, edges, preds, labels, cut=0.5, figsize=(16, 16)):
-    """"Draw Sample with True and False Edges"""
-    
+    """ "Draw Sample with True and False Edges"""
+
     # coordinate transformation
     r, phi, ir = hits.T
     x, y = polar_to_cartesian(r, phi)
-    
+
     # detector layout
     fig, ax = detector_layout(figsize=figsize)
-    
+
     # Draw the segments
     for j in range(labels.shape[0]):
-        
-        ptx1 = x[edges[0,j]]
-        ptx2 = x[edges[1,j]]
-        pty1 = y[edges[0,j]]
-        pty2 = y[edges[1,j]]
-        
+
+        ptx1 = x[edges[0, j]]
+        ptx2 = x[edges[1, j]]
+        pty1 = y[edges[0, j]]
+        pty2 = y[edges[1, j]]
+
         # False Negatives
         if preds[j] < cut and labels[j] > cut:
             # ax.plot([x[edges[0,j]], x[edges[1,j]]], [y[edges[0,j]], y[edges[1,j]]], '--', c='b')
-            ax.plot([ptx1, ptx2], [pty1, pty2], '--', color='b', lw=1.5, alpha=0.9)
-        
+            ax.plot([ptx1, ptx2], [pty1, pty2], "--", color="b", lw=1.5, alpha=0.9)
+
         # False Positives
         if preds[j] > cut and labels[j] < cut:
             # ax.plot([x[edges[0,j]], x[edges[1,j]]], [y[edges[0,j]], y[edges[1,j]]], '-', c='r', alpha=preds[j])
-            ax.plot([ptx1, ptx2], [pty1, pty2], '-', color='r', lw=1.5, alpha=0.15)
-        
+            ax.plot([ptx1, ptx2], [pty1, pty2], "-", color="r", lw=1.5, alpha=0.15)
+
         # True Positives
         if preds[j] > cut and labels[j] > cut:
             # ax.plot([x[edges[0,j]], x[edges[1,j]]], [y[edges[0,j]], y[edges[1,j]]], '-', c='k', alpha=preds[j])
-            ax.plot([ptx1, ptx2], [pty1, pty2], '-', color='k', lw=1.5, alpha=0.9)
-        
-    return fig, ax
+            ax.plot([ptx1, ptx2], [pty1, pty2], "-", color="k", lw=1.5, alpha=0.9)
 
+    return fig, ax
--- src/reader.py
+++ src/reader.py
@@ -24,9 +24,11 @@ except KeyError as e:
 
 
 # detector file
-detector_path = os.path.join(output_base, 'stt.csv')
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-Data = namedtuple('Data', ['hits', 'tubes', 'particles', 'truth', 'event', 'event_file'])
+detector_path = os.path.join(output_base, "stt.csv")
+device = "cuda" if torch.cuda.is_available() else "cpu"
+Data = namedtuple(
+    "Data", ["hits", "tubes", "particles", "truth", "event", "event_file"]
+)
 
 
 # SttTorchDataReader Class
@@ -54,7 +56,7 @@ class SttTorchDataReader(object):
 # SttCSVDataReader Class
 class SttCSVDataReader(object):
     """CSV Data Reader from an Input Directory. It compose and return full event."""
-    
+
     def __init__(self, input_dir: str, noise: bool, skewed: bool, selection: bool):
         """Initialize Instance Variables in Constructor"""
         self._path = input_dir
@@ -73,77 +75,86 @@ class SttCSVDataReader(object):
         all_files = sorted(glob.glob(os.path.join(self._path, "*")))
         self.nevts = len(all_files)
         self.all_evtids = [os.path.basename(x) for x in all_files]
-    
+
     def read(self, evtid: int = None):
         """Prepare event similar to event_utils/select_hit() in Processing."""
-        
+
         # create event_prefix from evtid
         prefix = "event{:010d}".format(evtid)
         event_prefix = os.path.join(os.path.expandvars(self._path), prefix)
-        
+
         try:
-            all_data = trackml.dataset.load_event(event_prefix)  # return with default order
+            all_data = trackml.dataset.load_event(
+                event_prefix
+            )  # return with default order
         except Exception as e:
             return e
-            
+
         if all_data is None:
             return False
-        
+
         hits, cells, particles, truth = all_data
-        
+
         # preprocess particles dataframe e.g. nhits, drop_duplicates, etc.
-        particles['nhits'] = particles.groupby(['particle_id'])['nhits'].transform('count')
+        particles["nhits"] = particles.groupby(["particle_id"])["nhits"].transform(
+            "count"
+        )
         particles.drop_duplicates(inplace=True, ignore_index=True)
-        
+
         if self._selection:
             # just keep protons, pions, don't forget resetting index and dropping old one.
-            particles = particles[particles['pdgcode'].isin([-2212, 2212, -211, 211])].reset_index(drop=True)
-        
+            particles = particles[
+                particles["pdgcode"].isin([-2212, 2212, -211, 211])
+            ].reset_index(drop=True)
+
         # merge some columns of tubes to the hits, I need isochrone, skewed & sector_id
-        hits = hits.merge(cells[["hit_id", "isochrone", "skewed", "sector_id"]], on="hit_id")
-    
+        hits = hits.merge(
+            cells[["hit_id", "isochrone", "skewed", "sector_id"]], on="hit_id"
+        )
+
         # add evtId to hits
         hits = hits.assign(event_id=evtid)
-        
+
         # add pT to particles
         px = particles.px
         py = particles.py
         pz = particles.pz
-        
+
         pt = np.sqrt(px**2 + py**2)
         p = np.sqrt(px**2 + py**2 + pz**2)
-        ptheta = np.arccos(pz/p)
-        peta = -np.log(np.tan(0.5*ptheta))
-        
+        ptheta = np.arccos(pz / p)
+        peta = -np.log(np.tan(0.5 * ptheta))
+
         particles = particles.assign(pt=pt, peta=peta)
-        
+
         # assign vars
         self._evtid = evtid
         self._hits = hits
         self._particles = particles
         self._truth = truth
         self._cells = cells
-        
+
         # compose event
         self.merge_truth_info_to_hits()
-        
+
         # prepare Data tuple
-        data = Data(self._hits, 
-                    self._cells, 
-                    self._particles, 
-                    self._truth, 
-                    self._event,  # merged event similar to select_hits()
-                    os.path.abspath(event_prefix)
-                    )
-        
+        data = Data(
+            self._hits,
+            self._cells,
+            self._particles,
+            self._truth,
+            self._event,  # merged event similar to select_hits()
+            os.path.abspath(event_prefix),
+        )
+
         return data
 
     def merge_truth_info_to_hits(self):
-        """Merge truth information ('truth', 'particles') to 'hits'. 
+        """Merge truth information ('truth', 'particles') to 'hits'.
         Then calculate and add derived variables to the event."""
-        
+
         hits = self._hits
-        
+
         # account for noise
         if self._noise:
             # runs if noise=True
@@ -151,21 +162,26 @@ class SttCSVDataReader(object):
         else:
             # runs if noise=False
             truth = self._truth.merge(self._particles, on="particle_id", how="inner")
-        
+
         # skip skewed tubes
         if self._skewed is False:
-            hits = hits.query('skewed==0')
-            
+            hits = hits.query("skewed==0")
+
             # rename layers from 0,1,2...,17 & assign to "layer" column
             vlids = hits.layer_id.unique()
             n_det_layers = len(vlids)
-            vlid_groups = hits.groupby(['layer_id'])
-            hits = pd.concat([vlid_groups.get_group(vlids[i]).assign(layer=i) for i in range(n_det_layers)])
+            vlid_groups = hits.groupby(["layer_id"])
+            hits = pd.concat(
+                [
+                    vlid_groups.get_group(vlids[i]).assign(layer=i)
+                    for i in range(n_det_layers)
+                ]
+            )
             self._hits = hits.reset_index(drop=True)
-            
+
         # merge 'hits' with 'truth'
-        hits = hits.merge(truth, on="hit_id", how='left')
-    
+        hits = hits.merge(truth, on="hit_id", how="left")
+
         # add new features to 'hits'
         x = hits.x
         y = hits.y
@@ -174,18 +190,17 @@ class SttCSVDataReader(object):
         r = np.sqrt(x**2 + y**2)  # in 2D
         r3 = np.sqrt(r**2 + z**2)  # in 3D
         phi = np.arctan2(hits.y, hits.x)
-        theta = np.arccos(z/r3)
-        eta = -np.log(np.tan(theta/2.))
+        theta = np.arccos(z / r3)
+        eta = -np.log(np.tan(theta / 2.0))
 
         tpx = hits.tpx
         tpy = hits.tpy
         tpt = np.sqrt(tpx**2 + tpy**2)
-        
+
         # add derived quantities to 'hits'
         hits = hits.assign(r=r, phi=phi, eta=eta, r3=r3, absZ=absz, tpt=tpt)
-        
+
         self._event = hits
-    
+
     def __call__(self, evtid: int, *args: Any, **kwds: Any) -> Any:
         return self.read(evtid)
-
